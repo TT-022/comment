d7c41a4e795a58c78c8724479abd1281186b3522
TT-022 <docongtuan0902@gmail.com>
2025-05-11 17:25:58 +0700

    app.py

--- app.py
+++ app.py
@@ -35,4 +35,4 @@ def index():
 
 if __name__ == "__main__":
     init_db()
-    app.run(debug=True,port=5001,use_reloader=False)
+    app.run(debug=True,host='0.0.0.0', port=int(os.environ.get('PORT', 5000)),use_reloader=False)
